f9b111b50b3ee01f44eb1cbce74a8ac93fa7b436
Cyril Pierron <cype59@gmail.com>
2019-11-25 11:33:12 +0100

    test widget

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar'
+import Widget from './widget'
+
 
 
 function App() {
   return (
     <div className="App">
         <Navbar></Navbar>
+        <Widget></Widget>
+        <Widget></Widget>
     </div>
   );
 }
