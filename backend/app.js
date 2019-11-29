ce18f46532a61713cd6a203a0e45f7bbd16d3d1a
Cyril Pierron <cype59@gmail.com>
2019-11-29 11:18:13 +0100

    first commit backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,18 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require('mongoose');
+const config = require('./config/database.config');
+
+// on se connecte à la base de données
+mongoose.connect(config.url, { useNewUrlParser: true });
+const app = express();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var measuresRouter = require("./routes/measures");
+var sensorsRouter = require("./routes/sensors");
+
 
 var app = express();
 
@@ -15,5 +24,8 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/measures", measuresRouter);
+app.use("/sensors", sensorsRouter);
+
 
 module.exports = app;
