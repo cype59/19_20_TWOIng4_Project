d2252898df2615176e402520cbd5530311900a3c
Schouketta <sarah.le@live.fr>
2019-11-30 19:31:38 +0100

    barre de navigation fonctionnelle git status!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,21 @@
 import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar'
-import WidgetBase from './Widgets/WidgetBase'
-import Header from './Components/Header'
-import WidgetChiffre from './Widgets/WidgetChiffre'
-import AdminPage from './Components/AdminPage';
 
 import { Container, Row, Col } from 'reactstrap';
+import {Link} from "react-router-dom";
+import PageDashboard from './Components/PageDashboard';
+import AdminPage from './Components/AdminPage';
+import Routes from './routes';
+import Navbar from './Components/Navbar';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="grilleWidget">
-        <Row>
-          <Col><Header /></Col>
-        </Row>
-        <Row>
-          <Col><WidgetChiffre nombre="8000" couleur = '#0088FE' intitule="Capteurs"></WidgetChiffre></Col>
-          <Col><WidgetChiffre nombre="352" couleur = '#00C49F' intitule="Utilisateurs"></WidgetChiffre></Col>
-          <Col><WidgetChiffre nombre="80" couleur = '#FFBB28' intitule="Capteurs"></WidgetChiffre></Col>
-          <Col><WidgetChiffre nombre="80" couleur = '#FF8042' intitule="Capteurs"></WidgetChiffre></Col>
-        </Row>
-        <Row>
-          <Col lg="4" sm="6"><WidgetBase nomWidget="MyBarChart" chartType= "barchart"></WidgetBase></Col>
-          <Col lg="4" sm="6"><WidgetBase nomWidget="MySpiderChart" chartType= "spiderchart"></WidgetBase></Col>
-          <Col lg="4" sm="12"><WidgetBase nomWidget="MyLineChart" chartType= "linechart"></WidgetBase></Col>
-        </Row>
-        <Row>
-          <Col lg="4" sm="12"><WidgetBase nomWidget="MyPieChart" chartType= "piechart"></WidgetBase></Col>
-          <Col lg="8" sm="12"><WidgetBase nomWidget="MyMapChart" chartType= "mapchart"></WidgetBase></Col>
-        </Row>
-      </div>
-      <Container>
-      <AdminPage/></Container>
+  <div>
+     <Navbar></Navbar>
+    <Routes/>
     </div>
+    
   );
 }
 
