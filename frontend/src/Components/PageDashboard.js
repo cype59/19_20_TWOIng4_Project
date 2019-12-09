d39472892f38f8829b5076dcd48ffd2c0983db71
Cyril Pierron <cype59@gmail.com>
2019-12-09 18:09:57 +0100

    widgetconnecté avec back

--- frontend/src/Components/PageDashboard.js
+++ frontend/src/Components/PageDashboard.js
@@ -6,20 +6,60 @@ import WidgetChiffre from '../Widgets/WidgetChiffre';
 
 import { Row, Col } from 'reactstrap';
 
+import axios from 'axios';
+
+
 class PageDashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usersCollection: [],
+            sensorsCollection: [],
+            measuresCollection: []
+        }
+        
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+            .then(res => {
+                console.log("response " + res)
+                this.setState({ usersCollection: res.data });
+            })
+            .catch(function (error) {
+                alert(error);
+            })
+        axios.get('http://localhost:3000/sensors')
+            .then(res => {
+                console.log("response " + res)
+                this.setState({ sensorsCollection: res.data });
+            })
+            .catch(function (error) {
+                alert(error);
+            })
+        axios.get('http://localhost:3000/measures')
+            .then(res => {
+                console.log("response " + res)
+                this.setState({ measuresCollection: res.data });
+            })
+            .catch(function (error) {
+                alert(error);
+            })
+    }
+
     render() {
         return (
 
             <div className="App">
-               
+
                 <div className="grilleWidget">
                     <Row>
                         <Col><Header /></Col>
                     </Row>
                     <Row>
-                        <Col><WidgetChiffre nombre="8000" couleur='#0088FE' intitule="Capteurs"></WidgetChiffre></Col>
-                        <Col><WidgetChiffre nombre="352" couleur='#00C49F' intitule="Utilisateurs"></WidgetChiffre></Col>
-                        <Col><WidgetChiffre nombre="80" couleur='#FFBB28' intitule="Capteurs"></WidgetChiffre></Col>
+                        <Col><WidgetChiffre nombre={this.state.usersCollection.length} couleur='#00C49F' intitule="Utilisateurs"></WidgetChiffre></Col>
+                        <Col><WidgetChiffre nombre={this.state.sensorsCollection.length} couleur='#0088FE' intitule="Capteurs"></WidgetChiffre></Col>
+                        <Col><WidgetChiffre nombre={this.state.measuresCollection.length} couleur='#FFBB28' intitule="Mesures"></WidgetChiffre></Col>
                         <Col><WidgetChiffre nombre="80" couleur='#FF8042' intitule="Capteurs"></WidgetChiffre></Col>
                     </Row>
                     <Row>
@@ -28,11 +68,11 @@ class PageDashboard extends Component {
                         <Col lg="4" sm="12"><WidgetBase nomWidget="MyLineChart" chartType="linechart"></WidgetBase></Col>
                     </Row>
                     <Row>
-                        <Col lg="4" sm="12"><WidgetBase nomWidget="MyPieChart" chartType="piechart"></WidgetBase></Col>
+                        <Col lg="4" sm="12"><WidgetBase nomWidget="MyPieChart" chartType="piechart" collection= {this.state.data}></WidgetBase></Col>
                         <Col lg="8" sm="12"><WidgetBase nomWidget="MyMapChart" chartType="mapchart"></WidgetBase></Col>
                     </Row>
                 </div>
-                
+
             </div>
 
         );
